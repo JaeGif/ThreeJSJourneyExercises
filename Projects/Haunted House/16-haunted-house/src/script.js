e7bbf1e726b7f5193de1a0d4810cd9d8752f2435
Jae Gifford <jae.giff@outlook.com>
2024-05-21 15:17:42 -0700

    changed rayleigh

--- Projects/Haunted House/16-haunted-house/src/script.js
+++ Projects/Haunted House/16-haunted-house/src/script.js
@@ -459,9 +459,9 @@ sky.scale.setScalar(100);
 scene.add(sky);
 
 sky.material.uniforms['turbidity'].value = 10;
-sky.material.uniforms['rayleigh'].value = 3;
+sky.material.uniforms['rayleigh'].value = 0.5;
 sky.material.uniforms['mieCoefficient'].value = 0.1;
-sky.material.uniforms['mieDirectionalG'].value = 0.95;
+sky.material.uniforms['mieDirectionalG'].value = 0.97;
 sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95);
 
 // Fog
