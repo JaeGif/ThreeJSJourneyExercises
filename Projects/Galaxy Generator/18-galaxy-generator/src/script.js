f910ced4ca280c7274298dc44085db73d65f8aa4
Jae <giffordjacob0@gmail.com>
2024-05-23 10:25:55 -0700

    adds increasing spread to particles

--- Projects/Galaxy Generator/18-galaxy-generator/src/script.js
+++ Projects/Galaxy Generator/18-galaxy-generator/src/script.js
@@ -16,37 +16,144 @@ const scene = new THREE.Scene();
 
 // Generate Galaxy
 const parameters = {
-  count: 1000,
+  count: 100000,
   color: 'yellow',
-  spread: 10,
-  size: 0.1,
-  attenuation: true,
+  randomnessPower: 3,
+  size: 0.01,
+  radius: 5,
+  branches: 3,
+  spin: 1,
+  randomness: 0.2,
+  insideColor: '#ff6030',
+  outsideColor: '#1b3984',
 };
 
-const generateGalaxy = () => {
-  const positions = new Float32Array(parameters.count * 3);
+let particlesGeometry = null;
+let particlesMaterial = null;
+let particles = null;
 
-  for (let i = 0; i < parameters.count * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * parameters.spread;
+const generateGalaxy = () => {
+  if (particles) {
+    // destroys the old stuff before remaking
+    // REQUIRED ELSE MEMORY LEAKS
+    particlesGeometry.dispose();
+    particlesMaterial.dispose();
+    scene.remove(particles);
+  }
+  const positions = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+
+  const colorInside = new THREE.Color(parameters.insideColor);
+  const colorOutside = new THREE.Color(parameters.outsideColor);
+
+  particlesGeometry = new THREE.BufferGeometry();
+  particlesMaterial = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
+  });
+
+  for (let i = 0; i < parameters.count; i++) {
+    // position
+
+    const i3 = i * 3;
+
+    const radius = Math.random() * parameters.radius;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const spinAngle = radius * parameters.spin;
+    // this is spread. Want spread to be more when bigger angle, less otherwise
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1) *
+      Math.cos(branchAngle + spinAngle) *
+      radius;
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1) *
+      radius;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      parameters.randomness *
+      (Math.random() < 0.5 ? 1 : -1) *
+      Math.sin(branchAngle + spinAngle) *
+      radius;
+
+    // when branch angle is higher less particles,
+
+    positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+    // color
+    const mixedColor = colorInside.clone();
+    mixedColor.lerp(colorOutside, radius / parameters.radius);
+    colors[i3 + 0] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
   }
   particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3)
   );
-  const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+  particles = new THREE.Points(particlesGeometry, particlesMaterial);
 
   scene.add(particles);
 };
+gui
+  .add(parameters, 'count')
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'size')
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'radius')
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'branches')
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'spin')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'randomness')
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, 'randomnessPower')
+  .min(1)
+  .max(10)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
+
 generateGalaxy();
 
 // Particles
-
-const particlesGeometry = new THREE.BufferGeometry();
-const particlesMaterial = new THREE.PointsMaterial({
-  color: parameters.color,
-  size: parameters.size,
-  sizeAttenuation: parameters.attenuation,
-});
 
 /**
  * Sizes
