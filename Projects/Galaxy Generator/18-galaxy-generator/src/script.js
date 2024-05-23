354e77f427f51d977ffb2413aa3a97774ee30544
Jae <giffordjacob0@gmail.com>
2024-05-23 08:52:13 -0700

    generate galaxy init

---
+++ Projects/Galaxy Generator/18-galaxy-generator/src/script.js
@@ -0,0 +1,119 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Generate Galaxy
+const parameters = {
+  count: 1000,
+  color: 'yellow',
+  spread: 10,
+  size: 0.1,
+  attenuation: true,
+};
+
+const generateGalaxy = () => {
+  const positions = new Float32Array(parameters.count * 3);
+
+  for (let i = 0; i < parameters.count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * parameters.spread;
+  }
+  particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+  );
+  const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+
+  scene.add(particles);
+};
+generateGalaxy();
+
+// Particles
+
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.color,
+  size: parameters.size,
+  sizeAttenuation: parameters.attenuation,
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight + 1,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight + 1;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
