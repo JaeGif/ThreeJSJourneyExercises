8af00cfd8c77b6fa88a649defa66966525c826c5
Jae <giffordjacob0@gmail.com>
2024-06-25 12:00:47 -0700

    check and add html

--- R3F/56-r3f-drei/src/Experience.jsx
+++ R3F/56-r3f-drei/src/Experience.jsx
@@ -1,33 +1,55 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+  Html,
+} from '@react-three/drei';
+import { useRef } from 'react';
 
-extend({ OrbitControls })
+export default function Experience() {
+  const cubeRef = useRef();
+  const sphereRef = useRef();
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
+  return (
+    <>
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-    return <>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        scale={0.5}
+        axisColors={['#9381ff', '#ff4d6d', '#7ae582']} // x | y | z
+      >
+        <mesh ref={sphereRef} position-x={-2}>
+          <Html
+            occlude={[sphereRef, cubeRef]}
+            center
+            position={[0, 1, 1]}
+            wrapperClass='label'
+            distanceFactor={8}
+          >
+            That's a sphere 👍
+          </Html>
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+          <sphereGeometry />
+          <meshStandardMaterial color='orange' />
         </mesh>
+      </PivotControls>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh ref={cubeRef} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color='mediumpurple' />
+      </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color='greenyellow' />
+      </mesh>
 
+      {/* Controls */}
+      <OrbitControls makeDefault />
+      <TransformControls object={cubeRef} />
     </>
+  );
 }
