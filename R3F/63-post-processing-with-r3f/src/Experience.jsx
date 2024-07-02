33060fba889ae25f36c2425cff0c6c5a9af44ae6
Jae <giffordjacob0@gmail.com>
2024-07-02 12:49:15 -0700

    push current position

--- R3F/63-post-processing-with-r3f/src/Experience.jsx
+++ R3F/63-post-processing-with-r3f/src/Experience.jsx
@@ -1,28 +1,56 @@
 import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
-import { EffectComposer } from '@react-three/postprocessing';
+import {
+  ToneMapping,
+  EffectComposer,
+  Vignette,
+  Glitch,
+  Noise,
+  Bloom,
+  DepthOfField,
+} from '@react-three/postprocessing';
+import { GlitchMode, ToneMappingMode } from 'postprocessing';
+import { BlendFunction } from 'postprocessing';
 
 export default function Experience() {
   return (
     <>
-      <EffectComposer></EffectComposer>
+      <color args={['#ffffff']} attach={'background'} />
+
+      <EffectComposer multisampling={8}>
+        {/*         <Vignette
+          offset={0.3}
+          darkness={0.6}
+          blendFunction={BlendFunction.NORMAL}
+        />
+        <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.05, 0.2]}
+          mode={GlitchMode.SPORADIC}
+        />
+        <Noise blendFunction={BlendFunction.SOFT_LIGHT} premultiply /> */}
+        {/*         <Bloom mipmapBlur luminanceThreshold={1.1} />
+         */}
+        <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        />
+        <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+      </EffectComposer>
       <Perf position='top-left' />
-
       <OrbitControls makeDefault />
-
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
-
       <mesh castShadow position-x={-2}>
         <sphereGeometry />
-        <meshStandardMaterial color='orange' />
+        <meshStandardMaterial color={'orange'} />
       </mesh>
-
       <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color='mediumpurple' />
+        <meshStandardMaterial color={'mediumpurple'} />
       </mesh>
-
       <mesh
         receiveShadow
         position-y={-1}
